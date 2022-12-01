12e6d9c118c267728c76d3c7af0b1d84d66a3d18
Bakul Mittal <bakul.mittal@axegroup.biz>
2022-12-01 11:09:35 +1100

    delete and get by id API

--- bank/src/main/java/com/infosys/bank/bankrestfulwebservices/BankRestfulWebservicesController.java
+++ bank/src/main/java/com/infosys/bank/bankrestfulwebservices/BankRestfulWebservicesController.java
@@ -2,9 +2,16 @@ package com.infosys.bank.bankrestfulwebservices;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +30,6 @@ public class BankRestfulWebservicesController {
 		this.bankAccountRepository = bankAccountRepository;
 	}
 
-	
 	@PostMapping("/createbankaccount")
 	public ResponseEntity<BankAccountEntity> createUser(@Valid @RequestBody BankAccountEntity bankAccountEntity) {
 
@@ -38,6 +44,26 @@ public class BankRestfulWebservicesController {
 	@GetMapping("/bankaccountusers")
 	public List<BankAccountEntity> retrieveAllUsers() {
 		return bankAccountRepository.findAll();
+	}
+
+	@GetMapping("/bankaccountusers/{id}")
+	public EntityModel<BankAccountEntity> retrieveUser(@PathVariable int id) {
+		Optional<BankAccountEntity> bankaccountuser = bankAccountRepository.findById(id);
+
+		if (bankaccountuser.isEmpty())
+			throw new BankAccountNotFoundException("id:" + id);
+
+		EntityModel<BankAccountEntity> entityModel = EntityModel.of(bankaccountuser.get());
+
+		WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+		entityModel.add(link.withRel("all-bak-account-users"));
+
+		return entityModel;
+	}
+
+	@DeleteMapping("/bankaccountusers/{id}")
+	public void deleteUser(@PathVariable int id) {
+		bankAccountRepository.deleteById(id);
 	}
 
 }
